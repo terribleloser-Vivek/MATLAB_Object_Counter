fd93bf86e421c235ebaca73d5b741360c0cdae64
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2021-07-09 17:25:24 +0530

    Uploaded image and matlab file

---
+++ my_script1.m
@@ -0,0 +1,27 @@
+﻿image = imread("A.jpg);
+imshow(image);
+
+%convert rgb image to gray image
+gray=rgb2gray(image)
+imshow(gray)
+
+%Histogram Plot
+imhist(gray)
+
+BW = gray>210;
+imshow(BW);
+
+%Submask creation radius 4
+se =strel('disk',4);
+
+%applying  mask onm image
+afteropening =imopen(BW,se);
+imshow(afteropening);l
+
+%to remove noise
+aftreclosing = imclose(BW,se);
+imshow(aftreclosing);
+
+imshowpair(image,afterclosing,'montage');
+
+[L,num] = bwlabel(~afterclosing,4);
